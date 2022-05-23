1b1b8dd6fe13306de28e00b2790d1a23625610a9
Grafein <83920425+Grafein@users.noreply.github.com>
2022-05-23 12:57:53 +0200

    Create EmptyException.java

---
+++ Programmazione II/intset/EmptyException.java
@@ -0,0 +1,11 @@
+package intset;
+
+import java.lang.*;
+
+
+public class EmptyException extends Exception {
+
+    public EmptyException() {super();}
+    public EmptyException(String s ) {super(s);}
+
+}
